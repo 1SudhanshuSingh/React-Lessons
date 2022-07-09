555c79ee46b021feec782cebd37a8419e7b25b72
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2022-07-09 18:57:40 +0530

    initial commit

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Hello</h1>
+      <p>Waiitttt</p>
+    </div>
+  );
+};
+
+export default Intro;
